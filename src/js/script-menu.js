8f397082912b698340566bb7b4796bff02c943ba
Kvitelev Vasiliy <kvitelevv@gmail.com>
2022-04-11 18:31:58 +0300

    Позиционирование элементов

--- src/js/script-menu.js
+++ src/js/script-menu.js
@@ -2,6 +2,16 @@ const btnOpen = document.querySelector('.header_btn');
 const menu = document.querySelector('.menu');
 const headerFone =document.querySelector('.header__wrapper');
 btnOpen.addEventListener ('click', function () {
-  menu.classList.toggle('menu');
-  headerFone.classList.toggle('header__wrapper--active');
+  if (btnOpen.classList.contains("header_btn--open")) {
+    menu.classList.remove('menu');
+    btnOpen.classList.add("header_btn--close");
+    btnOpen.classList.remove("header_btn--open");
+    headerFone.classList.add('header__wrapper--active');
+  }
+  else {
+    menu.classList.add('menu');
+    btnOpen.classList.remove("header_btn--close");
+    btnOpen.classList.add("header_btn--open");
+    headerFone.classList.remove('header__wrapper--active');
+  }
 })
